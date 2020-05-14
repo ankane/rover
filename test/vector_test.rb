9f3df44fcf5d17ae54a97e8614a1238a739ab2ce
Andrew Kane <andrew@chartkick.com>
2020-05-14 01:58:55 -0700

    Added test for endless range

--- test/vector_test.rb
+++ test/vector_test.rb
@@ -10,6 +10,7 @@ class VectorTest < Minitest::Test
     assert_equal 1, vector[0]
     assert_equal [1, 2], vector[0..1].to_a
     assert_equal [2, 3], vector[1..-1].to_a
+    assert_equal [2, 3], vector[1..nil].to_a # endless range
   end
 
   def test_array
