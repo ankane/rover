ac086a85bace6d8bdcd9d2619ab05053fd873caa
Andrew Kane <andrew@chartkick.com>
2020-06-08 18:11:59 -0700

    Cache groups

--- lib/rover/group.rb
+++ lib/rover/group.rb
@@ -1,58 +1,47 @@
 module Rover
   class Group
     def initialize(df, columns)
-      @df = df
-      @columns = Array(columns)
+      # cache here so we can reuse for multiple calcuations if needed
+      @grouped_dfs = create_groups(df, columns)
     end
 
-    def count
-      operation(:count)
-    end
-
-    [:max, :min, :mean, :median, :percentile, :sum].each do |name|
-      define_method(name) do |column, *args|
-        operation(name, column, *args)
+    [:count, :max, :min, :mean, :median, :percentile, :sum].each do |name|
+      define_method(name) do |*args|
+        result = {}
+        @grouped_dfs.each do |k, df|
+          result[k] = df.send(name, *args)
+        end
+        result
       end
     end
 
     private
 
-    def operation(method, *args)
-      check_columns(args.first)
-
-      result = {}
-      grouped_dfs.each do |k, df|
-        result[k] = df.send(method, *args)
-      end
-      result
-    end
-
     # TODO make more efficient
-    def grouped_dfs
+    def create_groups(df, columns)
+      columns = Array(columns)
+      raise ArgumentError, "No columns given" if columns.empty?
+      missing_keys = columns - df.keys
+      raise ArgumentError, "Missing keys: #{missing_keys.join(", ")}" if missing_keys.any?
+
       groups = Hash.new { |hash, key| hash[key] = [] }
-      if @columns.size == 1
-        @df[@columns.first].each_with_index do |v, i|
+      if columns.size == 1
+        df[columns.first].each_with_index do |v, i|
           groups[v] << i
         end
       else
         i = 0
-        @df.each_row do |row|
-          groups[@columns.map { |c| row[c] }] << i
+        df.each_row do |row|
+          groups[columns.map { |c| row[c] }] << i
           i += 1
         end
       end
 
       result = {}
       groups.each do |k, indexes|
-        result[k] = @df[indexes]
+        result[k] = df[indexes]
       end
       result
-    end
-
-    def check_columns(column)
-      raise ArgumentError, "No columns given" if @columns.empty?
-      missing_keys = @columns + [column].compact - @df.keys
-      raise ArgumentError, "Missing keys: #{missing_keys.join(", ")}" if missing_keys.any?
     end
   end
 end
