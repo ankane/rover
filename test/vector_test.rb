b0909df96a7259370f3deaf75bfe23c98334ec8d
Andrew Kane <andrew@chartkick.com>
2020-06-09 22:21:39 -0700

    Added test for to_object

--- test/vector_test.rb
+++ test/vector_test.rb
@@ -326,25 +326,33 @@ class VectorTest < Minitest::Test
     # assert_vector [1, 2, 3, 0], vector.to(:int)
     assert_kind_of Numo::Int64, vector.to(:int).to_numo
 
-    vector = Rover::Vector.new(["1", "2", "3"])
-    assert_vector [1, 2, 3], vector.to(:int)
+    vector = Rover::Vector.new(["1", "2", "3"]).to(:int)
+    assert_vector [1, 2, 3], vector
+    assert_equal :int, vector.type
     assert_kind_of Numo::Int64, vector.to(:int).to_numo
   end
 
   def test_to_float
-    vector = Rover::Vector.new(["1.0", "2.1", nil])
-    vector = vector.to(:float)
+    vector = Rover::Vector.new(["1.0", "2.1", nil]).to(:float)
     assert_equal vector[0], 1.0
     assert_equal vector[1], 2.1
     assert_equal vector[2].nan?, true
+    assert_equal :float, vector.type
     assert_kind_of Numo::DFloat, vector.to_numo
   end
 
   def test_to_bool
-    vector = Rover::Vector.new([1,2,0])
-    assert_vector [true, true, false], vector.to(:bool)
-    assert_equal :bool, vector.to(:bool).type
-    assert_kind_of Numo::Bit, vector.to(:bool).to_numo
+    vector = Rover::Vector.new([1, 2, 0]).to(:bool)
+    assert_vector [true, true, false], vector
+    assert_equal :bool, vector.type
+    assert_kind_of Numo::Bit, vector.to_numo
+  end
+
+  def test_to_object
+    vector = Rover::Vector.new(1..3).to(:object)
+    assert_vector [1, 2, 3], vector
+    assert_equal :object, vector.type
+    assert_kind_of Numo::RObject, vector.to_numo
   end
 
   def test_to_a
