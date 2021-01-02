7eef34ae74305af3567f801a6093a0fbac72ece2
Andrew Kane <andrew@ankane.org>
2021-01-01 19:25:12 -0800

    Added plot tests

--- test/data_frame_test.rb
+++ test/data_frame_test.rb
@@ -557,4 +557,43 @@ class DataFrameTest < Minitest::Test
   def test_arguments_types_keyword
     assert_equal [:types], Rover::DataFrame.new(types: {}).vector_names
   end
+
+  def test_plot
+    df = Rover::DataFrame.new({"a" => ["one", "two", "three"], "b" => [1, 2, 3]})
+    assert_plot_type "column", df.plot("a", "b")
+    assert_plot_type "scatter", df.plot("b", "b")
+  end
+
+  def test_plot_type
+    df = Rover::DataFrame.new({"a" => ["one", "two", "three"], "b" => [1, 2, 3]})
+    assert_plot_type "pie", df.plot("a", "b", type: "pie")
+    assert_plot_type "line", df.plot("a", "b", type: "line")
+    assert_plot_type "column", df.plot("a", "b", type: "column")
+    assert_plot_type "bar", df.plot("a", "b", type: "bar")
+    assert_plot_type "area", df.plot("a", "b", type: "area")
+    assert_plot_type "scatter", df.plot("b", "b", type: "scatter")
+  end
+
+  def test_plot_type_unknown
+    df = Rover::DataFrame.new({"a" => ["one", "two", "three"]})
+    error = assert_raises do
+      df.plot("a", "a")
+    end
+    assert_equal "Cannot determine type", error.message
+  end
+
+  def assert_plot_type(expected, plot)
+    assert_kind_of Vega::LiteChart, plot
+
+    case expected
+    when "column"
+      assert_equal "bar", plot.spec[:mark][:type]
+    when "pie"
+      assert_equal "arc", plot.spec[:mark][:type]
+    when "scatter"
+      assert_equal "circle", plot.spec[:mark][:type]
+    else
+      assert_equal expected, plot.spec[:mark][:type]
+    end
+  end
 end
