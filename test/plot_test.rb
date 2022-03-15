b6ffa21b0257e74cdf7ec4ed698114c616c15a8c
Andrew Kane <andrew@ankane.org>
2022-03-14 19:58:23 -0700

    Updated tests to check encoding for group option

--- test/plot_test.rb
+++ test/plot_test.rb
@@ -17,25 +17,21 @@ class PlotTest < Minitest::Test
     assert_plot_type "scatter", df.plot("b", "b", type: "scatter")
   end
 
-  def test_multiple_series
-    df = Rover::DataFrame.new({"category" => ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
-                              "group" => ["x", "y", "z", "x", "y", "z", "x", "y", "z"],
-                              "value" => [0.1, 0.6, 0.9, 0.7, 0.2, 1.1, 0.6, 0.1, 0.2]})
+  def test_group
+    df = Rover::DataFrame.new({"a" => ["one", "two", "three"], "b" => [1, 2, 3], "c" => ["group1", "group1", "group2"]})
+    assert_group df.plot("a", "b", type: "line", group: "c")
+    assert_group df.plot("a", "b", type: "column", group: "c")
+    assert_group df.plot("a", "b", type: "bar", group: "c")
+    assert_group df.plot("a", "b", type: "area", group: "c")
+    assert_group df.plot("b", "b", type: "scatter", group: "c")
+  end
 
+  def test_group_pie
+    df = Rover::DataFrame.new({"a" => ["one", "two", "three"], "b" => [1, 2, 3], "c" => ["group1", "group1", "group2"]})
     error = assert_raises(ArgumentError) do
-      df.plot("group", "value", type: "pie", group: "category")
+      df.plot("a", "b", type: "pie", group: "c")
     end
     assert_equal "Cannot use group option with pie chart", error.message
-
-    assert_plot_type "line", df.plot("group", "value", type: "line", group: "category")
-    assert_plot_type "column", df.plot("group", "value", type: "column", group: "category")
-    assert_plot_type "bar", df.plot("group", "value", type: "bar", group: "category")
-    assert_plot_type "area", df.plot("group", "value", type: "area", group: "category")
-
-    df = Rover::DataFrame.new({"category" => ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
-                              "group" => [1, 2, 3, 1, 2, 3, 1, 2, 3],
-                              "value" => [0.1, 0.6, 0.9, 0.7, 0.2, 1.1, 0.6, 0.1, 0.2]})
-    assert_plot_type "scatter", df.plot("group", "value", type: "scatter", group: "category")
   end
 
   def test_type_unknown
@@ -59,5 +55,10 @@ class PlotTest < Minitest::Test
     else
       assert_equal expected, plot.spec[:mark][:type]
     end
+  end
+
+  def assert_group(plot)
+    assert_kind_of Vega::LiteChart, plot
+    assert_equal "c", plot.spec[:encoding][:color][:field]
   end
 end
